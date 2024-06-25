1e0f52d195d2f7c0b127015c806c08ae022163cb
Pfingstrex <118078345+Pfingstrex@users.noreply.github.com>
2024-06-25 19:14:52 +0200

    Delete pfingstrex_disable_sprinting/lua/autorun/server directory

--- pfingstrex_disable_sprinting/lua/autorun/server/sv_animation_movement.lua
+++
@@ -1,9 +0,0 @@
-hook.Add("SetupMove", "Animation_Pfingstrex", function(ply, mv, cmd)
-    if ply:IsValid() and ply:Alive() then
-        if ply:KeyDown(IN_SPEED) and mv:GetForwardSpeed() < 0 then
-            ply:SetRunSpeed(ply:GetWalkSpeed())
-        else
-            ply:SetRunSpeed(240)
-        end
-    end
-end)
